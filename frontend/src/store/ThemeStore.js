9dbeb008f984353e4c9de50b21509d524667593e
Syed Sohail <sohailsyed7867123@gmail.com>
2025-06-28 00:36:24 +0530

    improved booking page

--- frontend/src/store/ThemeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const ThemeSet = create ((set)=>({
-     theme: localStorage.getItem("heyChat-theme") || "dark",
-     setTheme: (theme) =>{
-          localStorage.setItem("heyChat-theme", theme)
-          set({theme})
-     }
-}))
